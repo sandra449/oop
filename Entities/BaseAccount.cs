88fb8ab6e9e7ab609573edf584b585a141c353dc
sandra449 <sandaruismyname678@gmail.com>
2025-04-29 19:27:42 -0400

    Create BaseAccount.cs

---
+++ Entities/BaseAccount.cs
@@ -0,0 +1,10 @@
+
+namespace Entities
+{
+    public class BaseAccount
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+    }
+}
